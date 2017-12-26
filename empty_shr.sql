f6ef84b7f7da69a35b3cb19a4887c021ddf738be
Edgar Felizmenio <edgarfelizmenio@gmail.com>
2017-12-26 17:32:39 +0800

    Empty DB for empty SHR

---
+++ empty_shr.sql
@@ -0,0 +1,180 @@
+-- MySQL dump 10.13  Distrib 5.7.20, for Linux (x86_64)
+--
+-- Host: localhost    Database: raw_shr
+-- ------------------------------------------------------
+-- Server version	5.7.20-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Encounter`
+--
+
+DROP TABLE IF EXISTS `Encounter`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Encounter` (
+  `encounter_id` int(11) NOT NULL AUTO_INCREMENT,
+  `encounter_type` int(11) NOT NULL,
+  `patient_id` int(11) NOT NULL DEFAULT '0',
+  `location_id` int(11) DEFAULT NULL,
+  `encounter_datetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `creator` int(11) NOT NULL DEFAULT '0',
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`encounter_id`),
+  KEY `encounter_type_id` (`encounter_type`),
+  CONSTRAINT `encounter_type_id` FOREIGN KEY (`encounter_type`) REFERENCES `EncounterType` (`encounter_type_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=48845 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Observation`
+--
+
+DROP TABLE IF EXISTS `Observation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Observation` (
+  `obs_id` int(11) NOT NULL AUTO_INCREMENT,
+  `person_id` int(11) NOT NULL,
+  `concept_id` int(11) NOT NULL DEFAULT '0',
+  `encounter_id` int(11) DEFAULT NULL,
+  `obs_datetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `location_id` int(11) DEFAULT NULL,
+  `value_boolean` tinyint(1) DEFAULT NULL,
+  `value_coded` int(11) DEFAULT NULL,
+  `value_coded_name_id` int(11) DEFAULT NULL,
+  `value_datetime` datetime DEFAULT NULL,
+  `value_numeric` double DEFAULT NULL,
+  `comments` varchar(255) DEFAULT NULL,
+  `creator` int(11) NOT NULL DEFAULT '0',
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`obs_id`),
+  KEY `encounter_observations` (`encounter_id`),
+  CONSTRAINT `encounter_observations` FOREIGN KEY (`encounter_id`) REFERENCES `Encounter` (`encounter_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=476974 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `EncounterProvider`
+--
+
+DROP TABLE IF EXISTS `EncounterProvider`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `EncounterProvider` (
+  `encounter_provider_id` int(11) NOT NULL AUTO_INCREMENT,
+  `encounter_id` int(11) NOT NULL,
+  `provider_id` int(11) NOT NULL,
+  `encounter_role_id` int(11) NOT NULL,
+  `creator` int(11) NOT NULL DEFAULT '0',
+  `date_created` datetime NOT NULL,
+  PRIMARY KEY (`encounter_provider_id`),
+  KEY `encounter_id_fk` (`encounter_id`),
+  KEY `encounter_role_id_fk` (`encounter_role_id`),
+  CONSTRAINT `encounter_id_fk` FOREIGN KEY (`encounter_id`) REFERENCES `Encounter` (`encounter_id`),
+  CONSTRAINT `encounter_role_id_fk` FOREIGN KEY (`encounter_role_id`) REFERENCES `EncounterRole` (`encounter_role_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14317 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-12-26 17:30:23
+-- MySQL dump 10.13  Distrib 5.7.20, for Linux (x86_64)
+--
+-- Host: localhost    Database: raw_shr
+-- ------------------------------------------------------
+-- Server version	5.7.20-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `EncounterRole`
+--
+
+DROP TABLE IF EXISTS `EncounterRole`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `EncounterRole` (
+  `encounter_role_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(1024) DEFAULT NULL,
+  `creator` int(11) NOT NULL,
+  `date_created` datetime NOT NULL,
+  PRIMARY KEY (`encounter_role_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `EncounterRole`
+--
+
+LOCK TABLES `EncounterRole` WRITE;
+/*!40000 ALTER TABLE `EncounterRole` DISABLE KEYS */;
+INSERT INTO `EncounterRole` VALUES (1,'Unknown','Unknown encounter role for legacy providers with no encounter role set',1,'2011-08-18 14:00:00');
+/*!40000 ALTER TABLE `EncounterRole` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `EncounterType`
+--
+
+DROP TABLE IF EXISTS `EncounterType`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `EncounterType` (
+  `encounter_type_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL DEFAULT '',
+  `description` text,
+  `creator` int(11) NOT NULL DEFAULT '0',
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`encounter_type_id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `EncounterType`
+--
+
+LOCK TABLES `EncounterType` WRITE;
+/*!40000 ALTER TABLE `EncounterType` DISABLE KEYS */;
+INSERT INTO `EncounterType` VALUES (1,'ADULTINITIAL','Outpatient Adult Initial Visit',1,'2005-02-24 00:00:00'),(2,'ADULTRETURN','Outpatient Adult Return Visit',1,'2005-02-24 00:00:00'),(3,'PEDSINITIAL','Outpatient Pediatric Initial Visit',1,'2005-02-24 00:00:00'),(4,'PEDSRETURN','Outpatient Pediatric Return Visit',1,'2005-02-24 00:00:00');
+/*!40000 ALTER TABLE `EncounterType` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-12-26 17:30:30
