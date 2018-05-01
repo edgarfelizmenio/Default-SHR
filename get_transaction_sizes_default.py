59841840b1e03b66c4d9dd526ec0ebc7f128d518
Edgar Felizmenio <edgarfelizmenio@gmail.com>
2018-05-01 19:11:33 +0800

    get_transaction_sizes

---
+++ get_transaction_sizes_default.py
@@ -0,0 +1,56 @@
+import mysql.connector
+
+querystring = """
+	SELECT (encounter.size + obs.size) as transaction_size FROM
+	(SELECT encounter_id, (
+	LENGTH(encounter_id) +
+	LENGTH(encounter_type) +
+	LENGTH(patient_id) +
+	LENGTH(location_id) +
+	LENGTH(encounter_datetime) +
+	LENGTH(creator) +
+	LENGTH(date_created)
+	) AS size FROM Encounter) as encounter JOIN
+	(SELECT encounter_id, SUM(
+		LENGTH(obs_id) +
+		LENGTH(person_id) +
+		LENGTH(concept_id) +
+		LENGTH(encounter_id) +
+		LENGTH(obs_datetime) +
+		LENGTH(location_id) +
+		IFNULL(LENGTH(value_boolean), 0) +
+		IFNULL(LENGTH(value_coded), 0) +
+		IFNULL(LENGTH(value_coded_name_id), 0) +
+		IFNULL(LENGTH(value_datetime), 0) +
+		IFNULL(LENGTH(value_numeric), 0) +
+		IFNULL(LENGTH(comments), 0) +
+		LENGTH(creator) +
+		LENGTH(date_created)
+		) AS size FROM Observation
+		GROUP BY encounter_id) as obs ON encounter.encounter_id = obs.encounter_id;
+"""
+
+try:
+	connection = mysql.connector.connect(host='localhost',
+		port=3307,
+		database='raw_shr',
+		user='root',
+		password='password')
+	if connection.is_connected():
+		print('Connected to MySQL database')
+
+	cursor = connection.cursor()
+	cursor.execute(querystring)
+	rows = cursor.fetchall()
+
+	transaction_sizes = [int(txn[0]) for txn in rows]
+
+	with open('transaction_sizes_default.txt', "w") as txn_sizes_file:
+		txn_sizes_file.writelines(["{}\n".format(txn_size) for txn_size in transaction_sizes])
+
+except Error as e:
+	print(e)
+
+finally:
+	connection.close()
+	print('Connection closed.')
